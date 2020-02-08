17cd818a397034d49083a3f3c69178529975a922
Tommy Chiu <toms.cdxvi@gmail.com>
2020-02-08 13:16:00 -0500

    Delete myScript.js

--- myScript.js
+++
@@ -1,8 +0,0 @@
-console.log("just double checking");
-
-function login() {
-    var userName = document.getElementById('txtName').value;
-    
-    document.getElementById('welcomeMessage').textContent = "An email will be sent to your email shortly at" + " " + userName;
-    
-}
